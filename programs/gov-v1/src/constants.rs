785acf49aff87e7c101e5c6db6640fba8bd4c30b
iurage <iurage@protonmail.com>
2025-10-16 20:22:33 +0800

    Fixes FYEO-EXO-03
    Adds check for vec size limit (based on struct limits) when pushing

---
+++ programs/gov-v1/src/constants.rs
@@ -0,0 +1,2 @@
+pub const MAX_OPERATOR_VOTES: usize = 64;
+pub const MAX_BALLOT_TALLIES: usize = 64;
--- programs/gov-v1/src/lib.rs
+++ programs/gov-v1/src/lib.rs
@@ -1,6 +1,7 @@
 #![allow(ambiguous_glob_reexports)]
 #![allow(unexpected_cfgs)] // See: https://solana.stackexchange.com/a/19845
 
+pub mod constants;
 pub mod error;
 pub mod instructions;
 pub mod merkle_helper;
