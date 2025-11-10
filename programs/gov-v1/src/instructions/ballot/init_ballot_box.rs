2d9a1fb137cf2909ee3c043c23db1df737b1ba5b
iurage <iurage@protonmail.com>
2025-11-10 19:00:37 +0800

    Refactor InitBallotBox to use snapshot_slot and govcontract
    Changed InitBallotBox to use snapshot_slot for seeding and validation, replacing next_ballot_id. Replaced operator with govcontract and added a check to ensure snapshot_slot is greater than the current slot. Updated ballot_box initialization to include snapshot_slot and voter_list from program_config.

--- programs/gov-v1/src/instructions/ballot/init_ballot_box.rs
+++ programs/gov-v1/src/instructions/ballot/init_ballot_box.rs
@@ -1,47 +1,50 @@
 use anchor_lang::prelude::*;
 
-use crate::{BallotBox, ProgramConfig};
+use crate::{error::ErrorCode, BallotBox, ProgramConfig};
 
 #[derive(Accounts)]
+#[instruction(snapshot_slot: u64)]
 pub struct InitBallotBox<'info> {
     #[account(mut)]
     pub payer: Signer<'info>,
-    pub operator: Signer<'info>,
+    // TODO: Enforce check that signer is a PDA of gov contract program
+    pub govcontract: Signer<'info>,
     #[account(
         init,
         seeds = [
             b"BallotBox".as_ref(),
-            &program_config.next_ballot_id.to_le_bytes()
+            &snapshot_slot.to_le_bytes()
         ],
         bump,
         payer = payer,
         space = 8 + BallotBox::INIT_SPACE
     )]
     pub ballot_box: Box<Account<'info, BallotBox>>,
-    #[account(mut)]
     pub program_config: Box<Account<'info, ProgramConfig>>,
     pub system_program: Program<'info, System>,
 }
 
-pub fn handler(ctx: Context<InitBallotBox>) -> Result<()> {
-    let program_config = &mut ctx.accounts.program_config;
-    program_config.contains_operator(&ctx.accounts.operator.key())?;
-
-    let ballot_box = &mut ctx.accounts.ballot_box;
-    ballot_box.ballot_id = program_config.next_ballot_id;
-    ballot_box.bump = ctx.bumps.ballot_box;
-
+pub fn handler(ctx: Context<InitBallotBox>, snapshot_slot: u64) -> Result<()> {
     let clock = Clock::get()?;
+
+    // Check that snapshot slot is greater than current slot to 
+    // allow sufficient lead time for snapshot.
+    require!(snapshot_slot > clock.slot, ErrorCode::InvalidSnapshotSlot);
+
+    let program_config = &ctx.accounts.program_config;
+    let ballot_box = &mut ctx.accounts.ballot_box;
+
+    ballot_box.bump = ctx.bumps.ballot_box;
     ballot_box.epoch = clock.epoch;
     ballot_box.slot_created = clock.slot;
+    ballot_box.snapshot_slot = snapshot_slot;
     ballot_box.min_consensus_threshold_bps = program_config.min_consensus_threshold_bps;
     ballot_box.vote_expiry_timestamp = clock
         .unix_timestamp
         .checked_add(program_config.vote_duration)
         .unwrap();
-
-    // Increment for next ballot box
-    program_config.next_ballot_id = program_config.next_ballot_id.checked_add(1).unwrap();
+    ballot_box.voter_list = program_config.whitelisted_operators.clone();
+    ballot_box.tie_breaker_consensus = false;
 
     Ok(())
 }
